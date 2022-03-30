786a55972b82a2aca38aadf492557e2217e3b9b3
lee phan <pttl2k1@gmail.com>
2022-03-30 20:08:57 +0700

    Khoi tao du an

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,14 @@
+<script>
+export default {};
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <h1>Hello, Vue.js!</h1>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
 }
 </style>
